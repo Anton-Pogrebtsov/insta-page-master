afc4464725bb2856ee2c760b766ffd0a0190c155
Anton-Pogrebtsov <pogrebtsov.anton@gmail.com>
2018-11-04 20:16:33 +0400

    Первый коммит день второй
    дз второго дня

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){ 
 
-
+   var userFeed = new Instafeed({
+        get: 'user',
+        userId: '8798287512',
+        accessToken: '8798287512.1677ed0.a1028aad84e44495903272238f5d8510'
+    });
+    userFeed.run();
 
 });
